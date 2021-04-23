d40cc8ace4f381e285eec23206f1732cde9d3d4f
Naz Zebra <naz.zebra@gmail.com>
2021-04-23 15:34:31 +0300

    tooltips and home text update

--- js/documentation.js
+++ js/documentation.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
-  // code highlight
+  //////////////////////
+  // Code highlight
+  //////////////////////
   hljs.configure({languages: ['HTML']});
   hljs.highlightAll();
 
-  // Copy
-  function copyToClipboard(el) {
+  //////////////////////
+  // showTooltipFeedback
+  //////////////////////
+  function showTooltipFeedback(el, direction, text) {
+    var originalText = $(el).attr('data-tooltip-'+direction);
+    $(el).attr('data-tooltip-'+direction, text);
+    setTimeout(() => $(el).attr('data-tooltip-'+direction, originalText), 1000);
+  };
+
+  //////////////////////
+  // Copy Code
+  //////////////////////
+  function copyCodeToClipboard(el) {
     var text = el.siblings('pre').find('code').text();
     var input = document.createElement('textarea');
     input.innerHTML = text;
@@ -12,11 +25,46 @@ $(document).ready(function(){
     input.select();
     var result = document.execCommand('copy');
     document.body.removeChild(input);
+    // Feedback
+    showTooltipFeedback(el, 'left', 'Copied!');
     return result;
   }
 
   $('[data-copy-snippet').click(function(e) {
-    copyToClipboard($(this));
+    copyCodeToClipboard($(this));
   });
+
+  //////////////////////
+  // Add Links to H2
+  //////////////////////
+  const headings = document.querySelectorAll('.jekyll-content h2'); 
+  const linkContent = '<i class="icn icn-Linked"></i>';
+  for (const heading of headings) {
+    const linkIcon = document.createElement('a');
+    linkIcon.setAttribute('href', `#${heading.id}`);
+    linkIcon.setAttribute('data-tooltip-right', 'Copy link');
+    linkIcon.innerHTML = linkContent;
+    heading.appendChild(linkIcon);
+  }
+
+  function copyH2Link(el) {
+    var link = $(el).parent('h2').attr('id');
+    var input = document.createElement('textarea');
+    input.innerHTML = window.location.href + '#'+ link;
+    document.body.appendChild(input);
+    input.select();
+    var result = document.execCommand('copy');
+    document.body.removeChild(input);
+    // Feedback
+    showTooltipFeedback(el, 'right', 'Copied!');
+    return result;
+  }
+  $('.jekyll-content h2 a').click(function(e) {
+    e.preventDefault();
+    copyH2Link($(this));
+  });
+
+// End Doc Ready
 });
+
 
